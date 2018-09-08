88c6b3744a01b6e6cb66cdf10dd9cd7a893b425f
Tom Shaver <l3l_aze@yahoo.com>
2018-09-08 12:48:01 -0500

    God I hate Windows

--- install.bat
+++ install.bat
@@ -8,32 +8,36 @@ SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 :: variables
 set me=%~n0
 set parent=%~dp0
-set client='C:\Program Files (x86)\Steam\Steam.exe'
-set installer="https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe"
-set setup='SteamSetup.exe'
 
-if not exist "%client%" (
-  if not exist "%setup%" (
+if not exist "C:\\Program Files (x86)\\Steam" (
+  if not exist "SteamSetup.exe" (
     echo "Downloading Steam installer"
-    curl -fsSL -o "%setup%" "%installer%"
+    cd "%temp%"
+    curl -fSL -o SteamSetup.exe "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe"
   )
 
   echo "Running Steam installer"
-  cmd /c "%setup%" /S /V /qb
-  timeout 5
-  taskkill.exe /F /T /IM Steam* || ( cmd /c "exit /b 0" )
-
-  cmd /c "%client%" -silent || ( cmd /c "exit /b 0" )
-  cmd /c "%client%" -shutdown
-  timeout 15
+  SteamSetup.exe /S /V /qb
+  timeout 10
   taskkill.exe /F /T /IM Steam* || ( cmd /c "exit /b 0" )
 )
 
-start "%client%" "-login" "%steamu%" "%steamp%"
-timeout 90
-cmd /c "%client%" -shutdown
+cd "C:\\Program Files (x86)\\Steam"
+echo "Updating"
+Steam.exe
+timeout 15
+Steam.exe "-shutdown"
 timeout 15
 taskkill.exe /F /T /IM Steam* || ( cmd /c "exit /b 0" )
+
+echo "Logging in"
+cmd /c start Steam.exe "-login" "%steamu%" "%steamp%"
+timeout 45
+Steam.exe "-shutdown"
+echo "Exiting.."
+timeout 15
+taskkill.exe /F /T /IM Steam* || ( cmd /c "exit /b 0" )
+goto END
 
 :END
 ENDLOCAL
