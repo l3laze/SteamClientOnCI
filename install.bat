bdf5a7b4a5c00f5427c669610b7d25b6c35359d4
Tom Shaver <l3l_aze@yahoo.com>
2018-09-08 17:09:27 -0500

    Back to a batch script...

--- install.bat
+++ install.bat
@@ -10,30 +10,31 @@ set me=%~n0
 set parent=%~dp0
 
 if not exist "C:\\Program Files (x86)\\Steam" (
+  cd "%temp%"
+
   if not exist "SteamSetup.exe" (
     echo "Downloading Steam installer"
-    cd "%temp%"
     curl -fSL -o SteamSetup.exe "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe"
   )
 
   echo "Running Steam installer"
-  SteamSetup.exe /S /V /qb
+  start "" SteamSetup.exe /S /V /qb
   timeout 10
   taskkill.exe /F /T /IM Steam* || ( cmd /c "exit /b 0" )
 )
 
 cd "C:\\Program Files (x86)\\Steam"
 echo "Updating"
-cmd /c start Steam.exe
+Steam.exe
 timeout 45
-Steam.exe "-shutdown"
+start "" Steam.exe "-shutdown"
 timeout 15
 taskkill.exe /F /T /IM Steam* || ( cmd /c "exit /b 0" )
 
 echo "Logging in"
-cmd /c start Steam.exe "-login" "%steamu%" "%steamp%"
+start "" Steam.exe "-login" "%steamu%" "%steamp%"
 timeout 45
-Steam.exe "-shutdown"
+start "" Steam.exe "-shutdown"
 echo "Exiting.."
 timeout 15
 taskkill.exe /F /T /IM Steam* || ( cmd /c "exit /b 0" )
